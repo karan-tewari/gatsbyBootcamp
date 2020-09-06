6601b85067efa101ea5d97e8ef8cfa90f1b01f47
karan-tewari <ktewari601@gmail.com>
2020-09-06 20:17:33 +0530

    added blog posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import {gatsby, useStaticQuery, graphql} from 'gatsby';
 
 const Blog = () => {
+    let data = useStaticQuery(graphql`
+    query{
+        allMarkdownRemark{
+          edges{
+            node{
+              frontmatter{
+                title
+                date
+              }
+              excerpt
+              html
+            }
+          }
+        }
+      }
+    `)
     return(
         <Layout>
-            <p>Blog page</p>
+            <ol>
+                {data.allMarkdownRemark.edges.map(x => {
+                    return(
+                        <li key={x.node.frontmatter.title}>
+                    <h2>{x.node.frontmatter.title}</h2>
+                    <p>{x.node.frontmatter.date}</p>
+                </li>
+                    )
+                })}
+                
+            </ol>
         </Layout>
     )
 }
--- src/posts/react.md
+++ src/posts/react.md
@@ -8,3 +8,7 @@ I created this learn React post
 ##Tech Used
 
 1. Gatsby
+2. React
+3. Andrews Tutorial
+4. Docs
+5. Dedication to learn
